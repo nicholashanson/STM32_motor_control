22cd2df152c50eabe05e3cb3d688abed52f9a664
nicholashanson <nicholashanson27@outlook.com>
2025-06-27 15:25:32 +0100

    Refactored tests into .h/.c and built test_host.c to verify

--- Core/Inc/pwm_control.h
+++ Core/Inc/pwm_control.h
@@ -7,18 +7,8 @@ static uint32_t current_freq = 0;
 static float current_duty = 0.0f;
 
 uint16_t calculate_pwm_duty_cycle(uint8_t percent, uint16_t arr);
-
-void PWM_Setup(uint32_t frequency, float duty_cycle) {
-    current_freq = frequency;
-    current_duty = duty_cycle;
-}
-
-uint32_t PWM_GetFrequency(void) {
-    return current_freq;
-}
-
-float PWM_GetDutyCycle(void) {
-    return current_duty;
-}
+void PWM_Setup(uint32_t frequency, float duty_cycle);
+uint32_t PWM_GetFrequency(void);
+float PWM_GetDutyCycle(void);
 
 #endif // PWM_CONTROL_H
--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC = gcc
-CFLAGS = -ICore/Inc -IUnity -Wall -Wextra -std=c99
+CFLAGS = -ICore/Inc -ITest/Inc -IUnity -Wall -Wextra -std=c99
 
-SRC = Core/Src/pwm_control.c Unity/unity.c Test/test_pwm.c
+SRC = Core/Src/pwm_control.c Unity/unity.c Test/test_host.c Test/Src/test_pwm.c
 OUT = test_pwm
 
 all: $(OUT)
